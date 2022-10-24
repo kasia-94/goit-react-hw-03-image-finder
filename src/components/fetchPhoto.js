00dea7c1f9ae6490978c9bb9ffaa8586770f6403
Kateryna Berehovych <kasia-94@ukr.net>
2022-10-24 22:17:26 +0300

    1

--- src/components/fetchPhoto.js
+++ src/components/fetchPhoto.js
@@ -2,15 +2,8 @@ import axios from 'axios';
 
 const KEY = '29694222-7e0570b8fa43a8c6cfc4bb193';
 
-// export const fetchPhoto = async (searchQuery, page) => {
-//   return await axios.get(
-//     `https://pixabay.com/api/?q=cat&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//   );
-// };
-
 export function fetchPhoto(searchQuery, page) {
-  const response = axios.get(
+  return axios.get(
     `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
-  return response;
 }
